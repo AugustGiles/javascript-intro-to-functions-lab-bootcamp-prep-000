4118e6eb531fdbc964341546a935f7312a3dd4dd
Taylor August Giles <augustgiles8@gmail.com>
2018-02-26 07:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 function shout(string) {
+  console.log(string.toUpperCase())
   return string.toUpperCase()
 }
 
 function whisper(string) {
-  return string.toLowerCase()
-}
-function shout(string) {
   console.log(string.toUpperCase())
-}
-function whisper(string) {
-  console.log(string.toLowerCase())
+  return string.toLowerCase()
 }
